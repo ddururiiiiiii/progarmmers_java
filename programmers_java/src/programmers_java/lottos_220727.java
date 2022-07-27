7a8b0cc53d9ab45de5b3f5c5a2fd26febee4fd1d
ddururiiiiiii <o244227@daum.net>
2022-07-27 21:08:45 +0900

    로또의 최고 순위와 최저 순위

---
+++ programmers_java/src/programmers_java/lottos_220727.java
@@ -0,0 +1,61 @@
+package programmers_java;
+
+public class lottos_220727 {
+		
+	    public static int[] solution(int[] lottos, int[] win_nums) {
+	    	int[] answer = {};
+	    	answer = new int[2];
+	    	
+	    	int zeroCnt = 0;
+	    	int winCnt = 0;
+	    	
+	    	for (int i = 0; i < lottos.length; i++) {
+	    		// 0의 갯수 세기
+	    		if (lottos[i] == 0) {
+	    			zeroCnt++;
+	    			continue;
+	    		}
+	    		// 일치하는 숫자 세기
+	    		for (int j = 0; j < lottos.length; j++) {
+	    			if (lottos[i] == win_nums[j]) {
+	    				winCnt++;
+	    				break;
+	    			}
+	    		}
+	    	}
+	    	
+	    	int minCnt = winCnt;
+	    	int MaxCnt = winCnt + zeroCnt;
+	    	answer[1] = rank(minCnt);
+	    	answer[0] = rank(MaxCnt);
+	    	
+	    	System.out.println(Arrays.toString(answer));
+	    	return answer;
+	    }
+	    
+	    public static int rank(int cnt) {
+	    	
+	    	switch (cnt) {
+	    	case 6 :
+	    		return 1;
+	    	case 5 :
+	    		return 2;
+	    	case 4 :
+	    		return 3;
+	    	case 3 :
+	    		return 4;
+	    	case 2 :
+	    		return 5;
+	    	default :
+	    		return 6;
+	    	}
+	    }
+		
+		public static void main(String[] args) {
+		
+		int[] lottos = {44, 1, 0, 0, 31, 25};
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		solution(lottos, win_nums);
+		
+		}
+}
