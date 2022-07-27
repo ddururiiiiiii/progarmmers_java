0d075eb6fd7decb6a739af4c62d38661f64661cd
ddururiiiiiii <o244227@daum.net>
2022-07-27 22:22:02 +0900

    숫자 문자열과 영단어

---
+++ programmers_java/src/programmers_java/vocal_220727.java
@@ -0,0 +1,23 @@
+package programmers_java;
+
+public class vocal_220727 {
+	
+        public static int solution(String s) {
+        	int answer = 0;
+        	String arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            
+        	for (int i = 0; i < arr.length; i++) {
+        		s = s.replace(arr[i], Integer.toString(i));
+        	}
+            answer = Integer.parseInt(s);
+            System.out.println(answer);
+            return answer;
+        }
+    
+	
+	public static void main(String[] args) {
+	String s = "one4seveneight";
+	solution(s);
+	
+	}
+}
