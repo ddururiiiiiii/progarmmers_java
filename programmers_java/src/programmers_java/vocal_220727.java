78e646986beee47b645fc0dc38ccc860622d3eb2
ddururiiiiiii <o244227@daum.net>
2022-07-28 09:34:16 +0900

    없는 숫자 더하기

--- programmers_java/src/programmers_java/vocal_220727.java
+++ programmers_java/src/programmers_java/vocal_220727.java
@@ -2,22 +2,23 @@ package programmers_java;
 
 public class vocal_220727 {
 	
-        public static int solution(String s) {
-        	int answer = 0;
-        	String arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-            
-        	for (int i = 0; i < arr.length; i++) {
-        		s = s.replace(arr[i], Integer.toString(i));
-        	}
-            answer = Integer.parseInt(s);
-            System.out.println(answer);
-            return answer;
+    public static int solution(int[] numbers) {
+        int answer = 45;
+        
+        for (int i = 0; i < numbers.length; i++) {
+        	answer -= numbers[i];
+        }
+        
+        System.out.println(answer);
+        
+        return answer;
         }
     
 	
 	public static void main(String[] args) {
-	String s = "one4seveneight";
-	solution(s);
+//	int[] numbers = {1,2,3,4,6,7,8,0};
+		int[] numbers = {5,8,4,0,6,7,9};
+	solution(numbers);
 	
 	}
 }
