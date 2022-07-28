73a9f3d2956a49d6531f405885d33758e0751cdf
ddururiiiiiii <o244227@daum.net>
2022-07-28 19:20:45 +0900

    평균 구하기

---
+++ programmers_java/src/programmers_java/arrAvg_220728.java
@@ -0,0 +1,24 @@
+package programmers_java;
+
+public class arrAvg_220728 {
+	
+    public static double solution(int[] arr) {
+        double answer = 0;
+        
+        for (int i = 0 ; i < arr.length; i++) {
+        	answer += arr[i];
+        }
+        
+        answer = answer / arr.length;
+        
+        System.out.println(answer);
+        return answer;
+    }
+    
+	
+	public static void main(String[] args) {
+	int[] arr = {1, 2, 3, 4};
+	solution(arr);
+	
+	}
+}
