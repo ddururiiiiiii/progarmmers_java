ccd41c83769aa30dd4307bbf1b1898914f27d771
ddururiiiiiii <o244227@daum.net>
2022-07-28 11:03:54 +0900

    완주하지 못한 선수

---
+++ programmers_java/src/programmers_java/plusMinus_220728.java
@@ -0,0 +1,28 @@
+package programmers_java;
+
+import java.util.Arrays;
+
+public class plusMinus_220728 {
+	
+    public static String solution(String[] participant, String[] completion) {
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i; 
+        for (i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i]))
+               return participant[i];
+        }
+        System.out.println(participant[i]);
+        return participant[i];
+    }
+    
+	
+	public static void main(String[] args) {
+	String[] participant = {"leo", "kiki", "eden"};
+	String[] completion = {"eden", "kiki"};
+	solution(participant, completion);
+	
+	}
+}
