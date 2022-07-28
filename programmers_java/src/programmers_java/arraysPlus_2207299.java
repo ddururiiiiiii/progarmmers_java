78b152bec8fcb4ff9c4856201621093571888323
ddururiiiiiii <o244227@daum.net>
2022-07-28 18:08:57 +0900

    행렬의 덧셈

---
+++ programmers_java/src/programmers_java/arraysPlus_2207299.java
@@ -0,0 +1,17 @@
+package programmers_java;
+
+public class arraysPlus_2207299 {
+	
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+	String s = "-4";
+	
+	solution(s);
+	
+	}
+}
