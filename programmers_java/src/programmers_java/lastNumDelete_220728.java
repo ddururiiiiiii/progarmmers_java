e0273b3f13e6cb6ebd162968c4c01702b9a8027b
ddururiiiiiii <o244227@daum.net>
2022-07-29 13:35:34 +0900

    제일 작은 수 제거하기

---
+++ programmers_java/src/programmers_java/lastNumDelete_220728.java
@@ -0,0 +1,43 @@
+package programmers_java;
+
+import java.util.Arrays;
+
+public class lastNumDelete_220728 {
+	
+    public static int[] solution(int[] arr) {
+        
+        // 배열길이 1인경우
+       if (arr.length == 1) {
+    	   int[] answer = {-1};
+    	   return answer;
+       }
+       
+   	   int[] answer = new int[arr.length-1];
+   	   
+   	   // 제일 작은 값 구하기
+   	   int min = arr[0];
+   	   
+	   for (int i = 1; i < arr.length;  i++) {
+        	min = Math.min(min, arr[i]);
+        }
+	   
+	   int index = 0;
+	   
+	   for (int i = 0; i < arr.length;  i++) {
+		   if (arr[i] == min) {
+			   continue;
+		   }
+		   answer[index++] = arr[i];
+	   }
+	   
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+    
+	
+	public static void main(String[] args) {
+	int[] arr = {4,3,2,1};
+	solution(arr);
+	
+	}
+}
