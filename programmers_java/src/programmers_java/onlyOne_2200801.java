a6a675e00cb760cbed6de99a2dd91ce8f73d4877
ddururiiiiiii <o244227@daum.net>
2022-08-01 14:04:21 +0900

    나머지가 1이 되는 수 찾기

---
+++ programmers_java/src/programmers_java/onlyOne_2200801.java
@@ -0,0 +1,24 @@
+package programmers_java;
+
+public class onlyOne_2200801 {
+	
+    public static int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 2; i < n; i++) {
+        	if (n % i == 1) {
+        		answer = i;
+        		break;
+        	}
+        }
+        
+        System.out.println(answer);
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+	int n = 10;
+	solution(n);
+	
+	}
+}
