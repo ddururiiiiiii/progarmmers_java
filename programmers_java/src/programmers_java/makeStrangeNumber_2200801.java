56e77df7e27fb08ea21ac5ad6428743cc9bf6eee
ddururiiiiiii <o244227@daum.net>
2022-08-01 17:45:41 +0900

    이상한 문자 만들기

---
+++ programmers_java/src/programmers_java/makeStrangeNumber_2200801.java
@@ -0,0 +1,35 @@
+package programmers_java;
+
+import java.util.Arrays;
+
+public class makeStrangeNumber_2200801 {
+	
+    public static String solution(String s) {
+        String answer = "";
+        
+        String[] arr = s.split("");
+        System.out.println(Arrays.toString(arr));
+        
+        int index = 0;
+        for(int i = 0; i < arr.length; i++) {
+        	
+        	if(arr[i].equals(" ")) {
+        		index = 0;
+        	} else if(index % 2 == 0) {
+        		arr[i] = arr[i].toUpperCase();
+                index++;
+            } else if(index % 2 != 0) {
+            	arr[i] = arr[i].toLowerCase();
+                index++;
+            }
+            answer += arr[i];
+        } 
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+	String s = "try hello world";
+	solution(s);
+	
+	}
+}
