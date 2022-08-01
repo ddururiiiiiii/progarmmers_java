5efc37b3bab5c89359f21c675b0ad4f2c0fc0e76
ddururiiiiiii <o244227@daum.net>
2022-08-01 18:06:48 +0900

    정수 제곱근 판별

---
+++ programmers_java/src/programmers_java/makeSqrt_2200801.java
@@ -0,0 +1,21 @@
+package programmers_java;
+
+public class makeSqrt_2200801 {
+	
+    public static long solution(long n) {
+        long answer = -1;
+        
+        Double val = Math.sqrt(n);
+        
+        if (val == val.intValue()) {
+        	answer = (long) Math.pow(val+1, 2);
+        }
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+	long n = 121;
+	solution(n);
+	
+	}
+}
