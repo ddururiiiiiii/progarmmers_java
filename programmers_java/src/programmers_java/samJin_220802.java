f57266b6383121c0d6ffef788408103ffb34a15e
ddururiiiiiii <o244227@daum.net>
2022-08-02 21:59:14 +0900

    3진법

---
+++ programmers_java/src/programmers_java/samJin_220802.java
@@ -0,0 +1,30 @@
+package programmers_java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class samJin_220802 {
+	
+	public static int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> arrList = new ArrayList<>();
+        
+        while(n != 0) {
+        	arrList.add(n % 3);
+            n /= 3;
+        } 
+        
+        int temp = 1;
+        for(int i = arrList.size() - 1; i >= 0;i--) {
+            answer += arrList.get(i) * temp;
+            temp *= 3;
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+	int n = 45;
+	solution(n);
+	
+	}
+}
