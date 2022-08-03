471d0e49a241b47fa6eb3c1b29532af7393056a8
ddururiiiiiii <o244227@daum.net>
2022-08-03 22:13:27 +0900

    시저 암호

--- programmers_java/src/programmers_java/siger_220803.java
+++ programmers_java/src/programmers_java/siger_220803.java
@@ -2,24 +2,35 @@ package programmers_java;
 
 public class siger_220803 {
 
-    public static int solution(int[][] sizes) {
-        int answer = 0;
-        int max_v = 0;
-        int max_h = 0;
-        
-        for(int i = 0; i<sizes.length; i++){
-            int v = Math.max(sizes[i][0], sizes[i][1]);
-            int h = Math.min(sizes[i][0], sizes[i][1]);
-            max_v = Math.max(max_v,v);
-            max_h = Math.max(max_h,h);
-        }
-        
-        return answer = max_v * max_h;
-    }
+	public static String solution(String s, int n) {
+		String answer = "";
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == ' ') { 
+				answer += ch;
+				continue;
+			}
+			if (ch >= 'a' && ch <= 'z') {
+				if (ch + n > 'z') {
+					answer += (char) (ch - 26 + n);
+				} else {
+					answer += (char) (ch + n);
+				}
+			} else if (ch >= 'A' && ch <= 'Z') { 
+				if (ch + n > 'Z') {
+					answer += (char) (ch - 26 + n);
+				} else {
+					answer += (char) (ch + n);
+				}
+			}
+		}
+		return answer;
+	}
 
 	public static void main(String[] args) {
-		int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
-		solution(sizes);
+		String s = "AB";
+		int n = 1;
+		solution(s, n);
 
 	}
 }
