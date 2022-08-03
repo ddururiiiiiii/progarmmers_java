b8564c060a8c84fe3e2f1950296c507a959d4ff9
ddururiiiiiii <o244227@daum.net>
2022-08-03 22:31:29 +0900

    소수 찾기

--- programmers_java/src/programmers_java/findingSosoo_220801.java
+++ programmers_java/src/programmers_java/findingSosoo_220801.java
@@ -4,19 +4,18 @@ public class findingSosoo_220801 {
 	
     public static int solution(int n) {
         int answer = 0;
-        
-        // 1과 자기 자신으로만 나눠짐 
-        
-        int count = 0;
-        for (int i  = 2; i <= n; i++){
-        	if (i % 1 == 0 && i % i == 0) {
-        		count++;
-        		System.out.println(count);
-        	}
-        }
-        System.out.println(count);
-        
-        return answer;
+        for(int i = 2; i <= n; i++){
+            boolean flag = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+           	 if(i % j == 0)  { 
+           	   flag = false; 
+                      break; 
+           	  } 
+              }
+         if(flag == true) 
+            answer++; 
+       }
+      return answer;
     }
     
 	public static void main(String[] args) {
