9a2d449d781e6db366e8da22ecc1c1d48619e52f
ddururiiiiiii <o244227@daum.net>
2022-08-05 15:02:14 +0900

    없는 숫자 더하기 (복습)

---
+++ programmers_java/src/programmers_java2/lottoNum.java
@@ -0,0 +1,20 @@
+package programmers_java2;
+
+public class lottoNum {
+
+    public static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        
+        
+        
+        
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] numbers = { 44, 1, 0, 0, 31, 25 };
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		solution(numbers, win_nums);
+	}
+}
