c66fa088acf37745af6d309f4397b3c7e250f352
ddururiiiiiii <o244227@daum.net>
2022-08-06 13:11:24 +0900

    두 정수 사이의 합 (복습)

---
+++ programmers_java/src/programmers_java2/numBetweenNum.java
@@ -0,0 +1,25 @@
+package programmers_java2;
+
+public class numBetweenNum {
+
+    public static long solution(int a, int b) {
+        long answer = 0;
+        
+        if (a < b) 
+        	for (int i = a; i <= b; i++)
+        		answer += i;
+    	else if (a > b)
+        	for (int i = b; i <= a; i++)
+        		answer += i;
+    	else if (a == b)
+    		answer = a;
+        
+        return answer;
+    }
+    
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		solution(a, b);
+	}
+}
