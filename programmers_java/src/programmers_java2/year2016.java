b366efe47403c41f30a98ceccc58e1157fd257ca
ddururiiiiiii <o244227@daum.net>
2022-08-06 11:44:38 +0900

    문자열을 정수로 바꾸기 (복습)

--- programmers_java/src/programmers_java2/year2016.java
+++ programmers_java/src/programmers_java2/year2016.java
@@ -2,26 +2,14 @@ package programmers_java2;
 
 public class year2016 {
 
-    public static String solution(int a, int b) {
-        String answer = "";
-        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int[] mon = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        int days = 0;
-        int i;
-        for (i = 0; i < a-1; i++) {
-        	days += mon[i];
-        }
-        
-        days += b-1;
-        answer = day[days % 7];
-        
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
         return answer;
     }
 
 	public static void main(String[] args) {
-		int a = 5;
-		int b = 24;
-		solution(a, b);
+		String s = "1234";
+		solution(s);
 	}
 }
