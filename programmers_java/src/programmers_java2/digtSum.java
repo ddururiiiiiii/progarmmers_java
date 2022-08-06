08d6061b04f8bce5188587284098d9ee5c65b0df
ddururiiiiiii <o244227@daum.net>
2022-08-06 13:16:41 +0900

    자릿수 더하기 (복습)

---
+++ programmers_java/src/programmers_java2/digtSum.java
@@ -0,0 +1,20 @@
+package programmers_java2;
+
+public class digtSum {
+
+	public static int solution(int n) {
+		int answer = 0;
+		
+		while (n > 0){
+            answer += n % 10;
+			n /= 10;
+        }
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int n = 123;
+		solution(n);
+	}
+}
